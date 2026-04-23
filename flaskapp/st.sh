d6ef79077c98222062ef4bc39a3527d7259722a7
vzonov86-dot <vzonov86@gmail.com>
2026-04-23 17:10:44 +0500

    Fix st.sh: use python3, ensure gunicorn

--- flaskapp/st.sh
+++ flaskapp/st.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 gunicorn --bind 127.0.0.1:5000 some_app:app & APP_PID=$!
-sleep 5
-python client.py
+sleep 10
+python3 client.py
 APP_CODE=$?
 kill -TERM $APP_PID
+sleep 2
 exit $APP_CODE
